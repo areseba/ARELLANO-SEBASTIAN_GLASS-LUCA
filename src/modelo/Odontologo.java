f073819118cdd424a772e309ee308fd0dcddd265
areseba <areseba79@gmail.com>
2024-02-27 21:51:47 -0300

    odontologo

--- src/modelo/Odontologo.java
+++ src/modelo/Odontologo.java
@@ -1,4 +1,47 @@
 package modelo;
 
 public class Odontologo {
+    private Integer id;
+    private String matricula;
+    private String nombre;
+    private String appellido;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAppellido() {
+        return appellido;
+    }
+
+    public void setAppellido(String appellido) {
+        this.appellido = appellido;
+    }
+
+    public Odontologo(Integer id, String matricula, String nombre, String appellido) {
+        this.id = id;
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.appellido = appellido;
+    }
 }
